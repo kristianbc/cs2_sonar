3d11c54275f0b55a59e0b2bdb20d62867f3309fe
Kristian Pastucha <pastucha.kristian@ssinfotech.cz>
2025-03-06 09:46:45 +0100

    Update Offsets

--- cs2_sonar/Offsets.cs
+++ cs2_sonar/Offsets.cs
@@ -8,8 +8,8 @@ namespace cs2_sonar
 {
     public class Offsets
     {
-        public const int LocalPlayerPawn = 0x1889F20;
-        public const int dwEntityList = 0x1A359B0;
+        public const int LocalPlayerPawn = 0x188AF20;
+        public const int dwEntityList = 0x1A36A00;
         public const int m_hPlayerPawn = 0x80C;
         public const int m_VOldOrigin = 0x1324;
         public const int m_iIDEntIndex = 0x1458;
